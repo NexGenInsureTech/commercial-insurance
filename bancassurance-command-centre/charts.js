6bb00b035f3889a66f025715330e9595b49c09ea
nikashdeka <nikash.deka@gmail.com>
2026-03-11 00:37:44 +0530

    Added new version of Banca Command Centre with refactorization

--- bancassurance-command-centre/charts.js
+++ bancassurance-command-centre/charts.js
@@ -1,3 +1,42 @@
+// function drawChart(id, map) {
+//   const canvas = document.getElementById(id);
+
+//   const ctx = canvas.getContext("2d");
+
+//   const entries = Object.entries(map);
+
+//   if (entries.length === 0) return;
+
+//   const barHeight = 30;
+//   const spacing = 15;
+
+//   canvas.height = entries.length * (barHeight + spacing) + 60;
+//   canvas.width = 900;
+
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+//   const max = Math.max(...entries.map((e) => e[1]));
+
+//   entries.forEach((e, i) => {
+//     const label = e[0];
+//     const value = e[1];
+
+//     const y = i * (barHeight + spacing) + 40;
+
+//     const width = (value / max) * (canvas.width - 250);
+
+//     ctx.fillStyle = "#1f3b5c";
+
+//     ctx.fillRect(200, y, width, barHeight);
+
+//     ctx.fillStyle = "black";
+
+//     ctx.fillText(label, 10, y + 20);
+
+//     ctx.fillText(value.toLocaleString(), 210 + width, y + 20);
+//   });
+// }
+
 function drawChart(id, map) {
   const canvas = document.getElementById(id);
 
@@ -11,7 +50,9 @@ function drawChart(id, map) {
 
   const max = Math.max(...entries.map((e) => e[1]));
 
-  const barWidth = canvas.width / entries.length;
+  // const barWidth = canvas.width / entries.length;
+  const barWidth = Math.max(60, canvas.width / entries.length);
+  canvas.width = barWidth * entries.length;
 
   entries.forEach((e, i) => {
     const height = (e[1] / max) * 350;
